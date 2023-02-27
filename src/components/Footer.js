18f2b778ddcf6f4e02f4f8da22e3526ef19018dd
Andreea Ghenea <107212175+agh911@users.noreply.github.com>
2023-02-27 15:19:39 +0000

    added src folder

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return(
+        <footer className="text-center">
+            <p>&copy; Andreea G. 2023 - All Rights Reserved.</p>
+        </footer>
+    )
+}
+
+export default Footer;
