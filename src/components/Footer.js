297a7632422a8d4acf81bac492650b5ba84c5f28
Andreea Ghenea <andreeag@Andreeas-MBP.lan>
2023-03-02 13:17:04 +0000

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return(
-        <footer className="text-center">
-            <p>&copy; Andreea G. 2023 - All Rights Reserved.</p>
-        </footer>
-    )
-}
-
-export default Footer;
